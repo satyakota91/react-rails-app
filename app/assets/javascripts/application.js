05ccbe1072177da555d94002be956d75808a8911
Satya Kota <satya.kota@dentsplysirona.com>
2017-09-07 16:40:18 -0400

    second commit added record form

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,8 +10,12 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require turbolinks
 //= require jquery
+//= require jquery_ujs
 //= require bootstrap-sprockets
 //= require rails-ujs
-//= require turbolinks
+//= require react
+//= require react_ujs
+//= require components
 //= require_tree .
